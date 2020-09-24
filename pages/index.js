9885d84f78f34a59137aab7f7664c5d5946e23cd
Nursultan <nursultan_imanov@mail.ru>
2020-09-24 09:57:48 +0200

    prefetch data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>Hello World</div>
+import { withApollo } from '../lib/apollo'
+import { useQuery } from '@apollo/react-hooks'
+import USER_QUERY from '../queries/USER_QUERY.js'
+
+function Home() {
+  const { loading, error, data } = useQuery(USER_QUERY)
+  console.log(data)
+
+  return (
+    <div>
+      Hello World
+      <p>{data?.user[0].username}</p>
+    </div>
+  )
 }
+
+export default withApollo({ ssr: true })(Home)
